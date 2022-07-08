fd482ec386abb8353559ff66001902e4a118ed6e
maajid <maajid11102000@gmail.com>
2022-07-08 23:55:57 +0200

    finished day 3

--- react-app/30-days-of-react/src/index.js
+++ react-app/30-days-of-react/src/index.js
@@ -1,17 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import katfoto from './images/katfoto.jpg'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const katfototje = (
+  <div >
+    <img src={katfoto} alt="kat foto" width="100px" className="katfototje"></img>
+  </div>
+)
+
+const welcome = 'Welcome to je vader of react'
+const title = 'getting fucked by react'
+const subtitle = 'javascript library'
+const author ={
+  firstname: "Maajid",
+  lastname: "Saidy"
+}
+const date = "July 8th, 2022"
+
+const header = (
+  <header>
+    <div className="header-wrapper">
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Instructor: {author.firstname} {author.lastname}
+      </p>
+      <small>Date: {date}</small>
+    </div>
+  </header>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const numOne = 3
+const numTwo = 2
+
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne+numTwo}
+  </p>
+)
+
+const yearBorn = 2000
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {' '}
+    {author.firstname} {author.lastname} is {age} jaar oud
+  </p>
+)
+
+
+const formulier = (
+  <div className="formulier">
+    <h1>SUBSCRIBE</h1>
+    <p>sign up with your email address to receive news and updates</p>
+    <input placeholder="first name"></input>
+    <input placeholder="Last name"></input>
+    <input placeholder="Email"></input>
+    <br></br>
+    <button>Subscribe</button>
+  </div>
+)
+
+const techs = ['HTML', 'CSS', 'JS']
+const techsFormatted = techs.map((tech) => <li key="tech">{tech}</li>)
+const main = (
+  <main>
+    <p>prerequisite to get started {' '}</p>
+    <strong>
+      <em>react.js</em>
+    </strong>
+    :
+    <ul>
+     {techsFormatted}
+     {result}
+     {personAge}
+     {katfototje}
+     {formulier}
+    </ul>
+  </main>
+);
+
+const copyright = "copyright 2022"
+const footer = (
+  <footer>
+    <p>{copyright}</p>
+  </footer>
+);
+
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+);
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(app, rootElement);
